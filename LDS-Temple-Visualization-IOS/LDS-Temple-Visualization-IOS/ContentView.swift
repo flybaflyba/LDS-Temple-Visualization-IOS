616f93577213e65f1d8c3ff0ce3d155f316d5ff5
Litian Zhang <litian_zhang17@163.com>
2020-06-29 13:28:07 -1000

    Create new Project, LDS Temples IOS version is coming soon.

---
+++ LDS-Temple-Visualization-IOS/LDS-Temple-Visualization-IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LDS-Temple-Visualization-IOS
+//
+//  Created by Litian Zhang on 6/29/20.
+//  Copyright © 2020 Litian Zhang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
