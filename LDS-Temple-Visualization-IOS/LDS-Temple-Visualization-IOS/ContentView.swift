4ef3ac28cd877ad1b192c46042348f2a000d1204
Litian Zhang <litian_zhang17@163.com>
2020-06-29 14:10:59 -1000

    Design layout of initial screen

--- LDS-Temple-Visualization-IOS/LDS-Temple-Visualization-IOS/ContentView.swift
+++ LDS-Temple-Visualization-IOS/LDS-Temple-Visualization-IOS/ContentView.swift
@@ -8,11 +8,63 @@
 
 import SwiftUI
 
+//use screen Height to set how much space each view should take on the screen
+let screenWidth = UIScreen.main.bounds.size.width
+let screenHeight = UIScreen.main.bounds.size.height
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            TitleView().frame(width: screenWidth, height: screenHeight * 0.1, alignment: Alignment.center).background(Color.blue)
+            SpiralView().frame(width: screenWidth, height: screenHeight * 0.7, alignment: Alignment.center).background(Color.green)
+            YearDisplayView().frame(width: screenWidth, height: screenHeight * 0.05, alignment: Alignment.center).background(Color.blue)
+            SliderView().frame(width: screenWidth, height: screenHeight * 0.1, alignment: Alignment.center).background(Color.green)
+            SliderLabelView().frame(width: screenWidth, height: screenHeight * 0.05, alignment: Alignment.center).background(Color.blue)
+        }
+        
     }
 }
+
+
+
+
+struct TitleView: View {
+    var body: some View {
+        Text("LDS Temples")
+    }
+}
+
+struct SpiralView: View {
+    var body: some View {
+        Text("Spiral")
+    }
+}
+
+struct YearDisplayView: View {
+    var body: some View {
+        Text("Year display")
+    }
+}
+
+struct SliderView: View {
+    var body: some View {
+        Text("Slider")
+    }
+}
+
+struct SliderLabelView: View {
+    var body: some View {
+        Text("Slider lablel")
+    }
+}
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
