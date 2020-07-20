5284438842c459164ba4e8c726b54dbcb5b64f62
Litian Zhang <litian_zhang17@163.com>
2020-07-20 10:15:15 -1000

    Fix bug, for the last few temples, where their images are just with their names on it, we dont want to show the label because users can see the name on the images

--- LDS-Temple-Visualization-IOS/LDS-Temple-Visualization-IOS/MainScreenView.swift
+++ LDS-Temple-Visualization-IOS/LDS-Temple-Visualization-IOS/MainScreenView.swift
@@ -270,13 +270,13 @@ struct SpiralView: View {
 //                        .position(x: temple.x, y: temple.y + temple.size / 2 + 5)
 //                        .font(.system(size: 10))
 //                        .animation(sharedValues.hasAnimation ? sharedValues.myAnimation : sharedValues.myNoAnimation)
-//                          
+//
 //                }
 //            }
-//            
-//         
+//
+//
 //        }
-//        
+//
 //        return body
 //    }
     
@@ -318,7 +318,13 @@ struct SpiralView: View {
                 
                 ForEach(imageSpiralViewModel.onScreenTemples) { temple in
                     if temple.showName && sharedValues.animationInProgress == false {
-                        Text(temple.name)
+                        
+                        // handle the last few temples, where their images is just with their names on it,
+                        // we dont what to show the names, so we just used name as " "
+//                        if link == "no link" {
+//                            name = " "
+//                        }
+                        Text(temple.link == "no link" ? "" : temple.name)
                             .position(x: temple.x, y: temple.y + temple.size / 2 + 5)
                             .font(.system(size: 10))
                     }
