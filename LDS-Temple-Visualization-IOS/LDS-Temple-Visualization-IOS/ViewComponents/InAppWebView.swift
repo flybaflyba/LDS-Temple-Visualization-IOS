7e5802888c4b9e1ad1bccf1a1d8189657f634f5b
Litian Zhang <litian_zhang17@163.com>
2020-08-04 16:00:09 -1000

    Add in app webview message, if current temple does not have a link, we show the message and indicate how to return

--- LDS-Temple-Visualization-IOS/LDS-Temple-Visualization-IOS/ViewComponents/InAppWebView.swift
+++ LDS-Temple-Visualization-IOS/LDS-Temple-Visualization-IOS/ViewComponents/InAppWebView.swift
@@ -34,6 +34,7 @@ struct InAppWebView: View {
             // if the passed in text is no link, we will just display a message, instead of a webview 
             if url == "no link" {
                 Text("this.temple.does.not.have.a.website.yet") // we don't need this anymore, because we are showing a alert, see below
+                Text("slide.right.to.return")
             } else {
                 WebView(request: URLRequest(url: URL(string: url)!))
             }
